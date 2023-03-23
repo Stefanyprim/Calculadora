944752b92ccf136b969b3b0bbc07d2331ee5b537
stefany_prim <stefany_prim@estudante.sesisenai.org.br>
2023-03-23 08:48:58 -0300

    oii

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -18,30 +18,35 @@ public class MainActivity extends AppCompatActivity {
         n1 = findViewById(R.id.n1);
         n2 = findViewById(R.id.n2);
         Resultado = findViewById(R.id.Resultado);
-
+        Resultado.setVisibility(View.INVISIBLE);
     }
     public void somar(View v){
         int num1 = Integer.parseInt(n1.getText().toString());
         int num2 = Integer.parseInt(n2.getText().toString());
         int tl = num1 + num2;
+        Resultado.setVisibility(View.VISIBLE);
         Resultado.setText(tl+"");
     }
     public void subtrair(View v){
         int num1 = Integer.parseInt(n1.getText().toString());
         int num2 = Integer.parseInt(n2.getText().toString());
         int tl = num1 - num2;
+        Resultado.setVisibility(View.VISIBLE);
         Resultado.setText(tl+"");
     }
     public void multiplicar(View v){
         int num1 = Integer.parseInt(n1.getText().toString());
         int num2 = Integer.parseInt(n2.getText().toString());
         int tl = num1 * num2;
+        Resultado.setVisibility(View.VISIBLE);
         Resultado.setText(tl+"");
     }
     public void dividir(View v){
         int num1 = Integer.parseInt(n1.getText().toString());
         int num2 = Integer.parseInt(n2.getText().toString());
         int tl = num1/num2;
+        Resultado.setVisibility(View.VISIBLE);
         Resultado.setText(tl+"");}
+
 
 
