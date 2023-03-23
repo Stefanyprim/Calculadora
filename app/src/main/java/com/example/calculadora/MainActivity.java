d7826c06342028487d0458855837bd3397e41cae
stefany_prim <stefany_prim@estudante.sesisenai.org.br>
2023-03-23 08:46:47 -0300

    oii

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,13 +3,45 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText n1, n2;
+    TextView Resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        n1 = findViewById(R.id.n1);
+        n2 = findViewById(R.id.n2);
+        Resultado = findViewById(R.id.Resultado);
+
     }
-}
+    public void somar(View v){
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+        int tl = num1 + num2;
+        Resultado.setText(tl+"");
+    }
+    public void subtrair(View v){
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+        int tl = num1 - num2;
+        Resultado.setText(tl+"");
+    }
+    public void multiplicar(View v){
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+        int tl = num1 * num2;
+        Resultado.setText(tl+"");
+    }
+    public void dividir(View v){
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+        int tl = num1/num2;
+        Resultado.setText(tl+"");}
+
+
